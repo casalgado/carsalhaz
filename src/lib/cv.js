44a2c85b613a7cebe339b5575a0db73976f527b4
Carlos <casalgado86@gmail.com>
2023-08-20 11:17:17 -0500

    3fc demo

--- src/lib/cv.js
+++ src/lib/cv.js
@@ -26,4 +26,57 @@ const parseData = () => {
   });
 };
 
+const members_url =
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vRRmDmlVnTE5B2MQ6n76icC-UJwP20sOdJ_WM-EQVjrLcJuTwSmMqyWR0pkui4YJTSxvW9U52Nnfw_X/pub?gid=0&single=true&output=csv";
+const getMembers = () => {
+  let data = [];
+  return new Promise((resolve) => {
+    Papa.parse(members_url, {
+      download: true,
+      header: true,
+      complete: (results) => {
+        data = results.data;
+        // console.log(data);
+        resolve(data);
+      },
+    });
+  });
+};
+
+const orders_url =
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vRRmDmlVnTE5B2MQ6n76icC-UJwP20sOdJ_WM-EQVjrLcJuTwSmMqyWR0pkui4YJTSxvW9U52Nnfw_X/pub?gid=1607532012&single=true&output=csv";
+const getOrders = () => {
+  let data = [];
+  return new Promise((resolve) => {
+    Papa.parse(orders_url, {
+      download: true,
+      header: true,
+      complete: (results) => {
+        data = results.data;
+        // console.log(data);
+        resolve(data);
+      },
+    });
+  });
+};
+
+const products_url =
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vRRmDmlVnTE5B2MQ6n76icC-UJwP20sOdJ_WM-EQVjrLcJuTwSmMqyWR0pkui4YJTSxvW9U52Nnfw_X/pub?gid=1511137527&single=true&output=csv";
+const getProducts = () => {
+  let data = [];
+  return new Promise((resolve) => {
+    Papa.parse(products_url, {
+      download: true,
+      header: true,
+      complete: (results) => {
+        data = results.data;
+        // console.log(data);
+        resolve(data);
+      },
+    });
+  });
+};
 export const cv_data = parseData();
+export const members = getMembers();
+export const orders = getOrders();
+export const products = getProducts();
--- src/router/index.js
+++ src/router/index.js
@@ -33,6 +33,14 @@ const router = createRouter({
       component: () => import("../views/ContactView.vue"),
     },
     {
+      path: "/3fc",
+      name: "3fc",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/ClubData.vue"),
+    },
+    {
       path: "/work/esalimento",
       name: "esalimento",
       // route level code-splitting
